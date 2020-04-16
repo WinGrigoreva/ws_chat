f27e8c8900ec9f30af4ce670f67d14e164019adb
WinGrigoreva <kriviktoria@gmail.com>
2020-04-16 19:55:19 +0300

    version 3

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 let wss = require("ws").Server;
-let server = new wss({port:591}, function() {
+let fs = require("fs");
+let messages = JSON.parse(fs.readFileSync("history.json","utf-8"));
+let server = new wss({port:591},function(){
     console.log("Сервер запущен!");
 });
 let clients = new Set();
 
 server.on("connection", function(socket){    
     clients.add(socket);
+    socket.send(JSON.stringify(messages));
     socket.on("message", function(message){
+        messages = messages.concat(JSON.parse(message));
         for (let i of clients){
             i.send(message);
         }
+        fs.writeFileSync("history.json", JSON.stringify(messages));
     });
     socket.on("close", function(){
         clients.delete(socket);
--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let text     = $("#text"),
     ws;
 
 chatname.change(function (event) {
+    $(".message").remove();
+    console.log(message);
     if (ws) {
         ws.close();
         ws = null;
@@ -15,21 +17,26 @@ chatname.change(function (event) {
 
     ws.onmessage = function (message_ws) {  
         let message_in = JSON.parse(message_ws.data);
-        let message_field = `<div class="message alert alert-light ${message_in.name == name.val() ? "message-right" : "message-left"}">
-                                <div class="username">${message_in.name}</div>
-                                <div class="usertext">${message_in.text}</div>
-                            </div>`;
+        console.log(message_in);
+        message_in.forEach(element => {
+            let message_field = `<div class="message alert alert-light ${element.name == name.val() ? "message-right" : "message-left"}">
+                                    <div class="username">${element.server}: ${element.name}</div>
+                                    <div class="usertext">${element.text}</div>
+                                </div>`;
         chat.append(message_field);
+        }) 
+        
         chat_body.scrollTop(chat.height());
     };
 
     ws.onopen = function () {
         text.keyup(function (event) {
             if (event.which === 13 && name.val() && text.val()) {
-                let message_out = {
+                let message_out = [{
+                    server: $("#chatname option:selected").text(),
                     name: name.val(),
                     text: text.val(),
-                }
+                }];
                 ws.send(JSON.stringify(message_out));
                 text.val("");
             }
